75da65fb2528504d4928a534c7e2c3b4854a3b9e
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-09 10:08:36 +0700

    Conform config with separated env

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT
+    "username": process.env.DEVELOPMENT_DB_USERNAME,
+    "password": process.env.DEVELOPMENT_DB_PASSWORD,
+    "database": process.env.DEVELOPMENT_DB_NAME,
+    "host": process.env.DEVELOPMENT_DB_HOST,
+    "port": process.env.DEVELOPMENT_DB_PORT,
+    "dialect": process.env.DEVELOPMENT_DB_DIALECT
   },
   "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT
+    "username": process.env.TEST_DB_USERNAME,
+    "password": process.env.TEST_DB_PASSWORD,
+    "database": process.env.TEST_DB_NAME,
+    "host": process.env.TEST_DB_HOST,
+    "port": process.env.TEST_DB_PORT,
+    "dialect": process.env.TEST_DB_DIALECT
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT
+    "username": process.env.PRODUCTION_DB_USERNAME,
+    "password": process.env.PRODUCTION_DB_PASSWORD,
+    "database": process.env.PRODUCTION_DB_NAME,
+    "host": process.env.PRODUCTION_DB_HOST,
+    "port": process.env.PRODUCTION_DB_PORT,
+    "dialect": process.env.PRODUCTION_DB_DIALECT
   }
 }
