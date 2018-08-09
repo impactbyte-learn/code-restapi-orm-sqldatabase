b6419f5b44d410c0a8c0fc01e2d4b422cdcbb99a
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-09 10:44:50 +0700

    Set render to send

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({
+    err
+  });
 });
 
 module.exports = app;
