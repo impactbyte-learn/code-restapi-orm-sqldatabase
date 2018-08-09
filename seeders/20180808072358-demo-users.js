708667268af0287eb74933f44f6c11477d98d8fe
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-09 10:45:23 +0700

    Refine demo users

--- seeders/20180808072358-demo-users.js
+++ seeders/20180808072358-demo-users.js
@@ -8,28 +8,58 @@ module.exports = {
     */
 
     const demoUsers = [{
-      username: 'mhaidarh',
-      email: 'haidar@impactbyte.com',
+      username: 'haidar',
+      email: 'haidar@example.com',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: 'abbayosua',
-      email: 'email@abbayosua.com',
+      email: 'abba@example.com',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: 'fabilqis',
-      email: 'fadillah.bilqis@gmail.com',
+      email: 'fadillah.bilqis@example.com',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: 'gunturkh',
-      email: 'guntur.kh@gmail.com',
+      email: 'guntur.kh@example.com',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       username: 'indrolie',
-      email: 'indrolie@gmail.com',
+      email: 'indrolie@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      username: 'mayasopiee',
+      email: 'mayasopiee@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      username: 'alifraher',
+      email: 'alifraher@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      username: 'ariebrainware',
+      email: 'ariebrainware@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      username: 'wondoindra',
+      email: 'wondoindra@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      username: 'michaeltamsil',
+      email: 'michaeltamsil@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      username: 'amirul-inc',
+      email: 'amirul@example.com',
       createdAt: new Date(),
       updatedAt: new Date()
     }]
