5b379cef327cf149672295944daeea2d09551456
M Haidar Hanif <mhaidarhanif@gmail.com>
2018-08-09 15:55:54 +0700

    Refactor models usage to use its object directly

--- api/users/controller.js
+++ api/users/controller.js
@@ -1,45 +1,47 @@
 const models = require('../../models')
+const User = models.User
 
 const controller = {
-    get: (req, res, next) => {
-        models.User
-            .findAll().then(users => {
-                res.send({
-                    users
-                });
-            }).catch(error => {
-                res.status(400).send({
-                    error
-                })
-            })
-    },
-
-    getOneById: (req, res, next) => {
-        const id = Number(req.params.id)
-        models.User
-            .findById(id)
-            .then(user => {
-                if (user) {
-                    res.send({
-                        user
-                    })
-                } else {
-                    res.send({
-                        message: `User not found`
-                    })
-                }
-            }).catch(error => {
-                res.status(400).send({
-                    error
-                })
-            })
-    },
-
-    create: (req, res, next) => {
+  get: (req, res, next) => {
+    User.findAll()
+      .then(users => {
         res.send({
-            users
-        });
-    }
+          users
+        })
+      })
+      .catch(error => {
+        res.status(400).send({
+          error
+        })
+      })
+  },
+
+  getOneById: (req, res, next) => {
+    const id = Number(req.params.id)
+    User.findById(id)
+      .then(user => {
+        if (user) {
+          res.send({
+            user
+          })
+        } else {
+          res.send({
+            message: 'User not found'
+          })
+        }
+      })
+      .catch(error => {
+        res.status(400).send({
+          error
+        })
+      })
+  },
+
+  create: (req, res, next) => {
+    res.send({
+      users
+    })
+  }
 }
 
 module.exports = controller
