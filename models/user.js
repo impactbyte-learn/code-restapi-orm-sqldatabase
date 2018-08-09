b0f8da19da1a0c6a9337b2fb0b123e1183be064d
M Haidar Hanif <mhaidarhanif@gmail.com>
2018-08-09 14:41:21 +0700

    Prettier models files

--- models/user.js
+++ models/user.js
@@ -1,16 +1,20 @@
-'use strict';
+'use strict'
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+      },
+      username: DataTypes.STRING,
+      email: DataTypes.STRING
     },
-    username: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
-  User.associate = function (models) {
+    {}
+  )
+  User.associate = function(models) {
     // associations can be defined here
-  };
-  return User;
-};
+  }
+  return User
+}
